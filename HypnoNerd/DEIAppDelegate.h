646ed881902383df179e0f216ae6ba6632fb6b88
gdsanders <gd@digitalequity.com>
2015-06-17 21:55:06 -0500

    Initial Commit

---
+++ HypnoNerd/DEIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEIAppDelegate.h
+//  HypnoNerd
+//
+//  Created by G.D. Sanders on 6/17/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by G.D. Sanders on 6/17/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEIAppDelegate class]));
+    }
+}
